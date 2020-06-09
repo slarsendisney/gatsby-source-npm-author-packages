a5898bf4ae35a6a7129eb3c9f73d6ad9868531da
slarsendisney <s.larsendisney@gmail.com>
2020-06-09 17:17:00 +0200

    init

---
+++ gatsby-node.js
@@ -0,0 +1,27 @@
+const crypto = require("crypto")
+const search = require("libnpmsearch")
+
+exports.sourceNodes = async ({ actions }, configOptions) => {
+  const { createNode } = actions
+  const data = await search(`author:${configOptions.author}`)
+  for (let result of data) {
+    const { name, description, links, version, keywords } = result
+    createNode({
+      name,
+      description,
+      keywords,
+      links,
+      version,
+      id: name,
+      internal: {
+        type: `NPMPackage`,
+        contentDigest: crypto
+          .createHash(`md5`)
+          .update(JSON.stringify({ name, description, links, version }))
+          .digest(`hex`),
+        mediaType: `text/plain`,
+        description: `NPM package data`,
+      },
+    })
+  }
+}
